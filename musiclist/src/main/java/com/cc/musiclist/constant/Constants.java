07fcf0cbd7e84c916dda0363ad9891191571904f
zhangyuChen <742352368@qq.com>
2016-06-19 17:40:29 +0800

    Merge branch 'master' of E:\AndroidStudioProjects\OwnApplication with conflicts.

---
+++ musiclist/src/main/java/com/cc/musiclist/constant/Constants.java
@@ -0,0 +1,7 @@
+package com.cc.musiclist.constant;
+
+/**
+ * Created by zhangyu on 2016/6/19 16:28.
+ */
+public class Constants {
+}
---
+++ musiclist/src/main/java/com/cc/musiclist/manager/MediaPlayManager.java
@@ -0,0 +1,7 @@
+package com.cc.musiclist.manager;
+
+/**
+ * Created by zhangyu on 2016/6/19 14:51.
+ */
+public class MediaPlayManager {
+}
