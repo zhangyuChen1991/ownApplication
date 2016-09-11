6463c9c79fdb686ff318b88b838da52d6587cc36
zhangyuChen <742352368@qq.com>
2016-09-11 12:30:42 +0800

    rename file

---
+++ musicList/src/main/java/com/cc/musiclist/adapter/TabsAdapter.java
@@ -0,0 +1,30 @@
+package com.cc.musiclist.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by zhangyu on 2016-06-30 19:29.
+ */
+public class TabsAdapter extends FragmentPagerAdapter {
+    private static final String TAG = "TabsAdapter";
+    private List<Fragment> fragmentList;
+
+    public TabsAdapter(FragmentManager fm, List<Fragment> fragmentList) {
+        super(fm);
+        this.fragmentList = fragmentList;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+}
